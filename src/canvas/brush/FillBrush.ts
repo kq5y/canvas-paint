a6608c7f0db5fc1a6630e98c15ef7d1f56a2305f
tkser <tkser.edu@gmail.com>
2024-04-04 23:21:11 +0900

    fix: filling alpha

--- src/canvas/brush/FillBrush.ts
+++ src/canvas/brush/FillBrush.ts
@@ -27,7 +27,7 @@ export class FillBrush extends BrushBase {
     }
 
     _pixelsEqual(p1: number[], p2: number[]): boolean {
-        return p1[0] === p2[0] && p1[1] === p2[1] && p1[2] === p2[2];
+        return p1[0] === p2[0] && p1[1] === p2[1] && p1[2] === p2[2] && p1[3] === p2[3];
     }
 
     _compareColor(
@@ -35,23 +35,20 @@ export class FillBrush extends BrushBase {
         x: number,
         y: number,
         selectedColor: number[],
-        isAlpha: boolean
-    ): boolean {
-        if (x < 0 || x >= this._width || y < 0 || y >= this._height) return false;
+        targetColor: number[]
+    ): 0 | 1 | 2 {
+        if (x < 0 || x >= this._width || y < 0 || y >= this._height) return 1;
 
         const color = new Array(3);
         color[0] = imageData.data[(y * this._width + x) * 4];
         color[1] = imageData.data[(y * this._width + x) * 4 + 1];
         color[2] = imageData.data[(y * this._width + x) * 4 + 2];
+        color[3] = imageData.data[(y * this._width + x) * 4 + 3];
 
-        const alpha = imageData.data[(y * this._width + x) * 4 + 3];
+        if (this._pixelsEqual(targetColor, color)) return 1;
+        if (!this._pixelsEqual(selectedColor, color)) return 2;
 
-        if (alpha !== 0) {
-            if (!this._pixelsEqual(selectedColor, color)) return false;
-            if (isAlpha) return false;
-        } else if (alpha === 0 && !isAlpha) return false;
-
-        return true;
+        return 0;
     }
 
     _fill(point: { x: number; y: number }): void {
@@ -60,45 +57,68 @@ export class FillBrush extends BrushBase {
         const x = Math.floor(point.x),
             y = Math.floor(point.y);
         const imageData = this._context.getImageData(0, 0, this._width, this._height);
-        const color = [...rgbaStr2Array(this._color), this._opacity / 100];
+        const color = [...rgbaStr2Array(this._color).slice(0, 3), (this._opacity / 100) * 255];
 
         const selectedColor = new Array(3);
         selectedColor[0] = imageData.data[(y * this._width + x) * 4];
         selectedColor[1] = imageData.data[(y * this._width + x) * 4 + 1];
         selectedColor[2] = imageData.data[(y * this._width + x) * 4 + 2];
+        selectedColor[3] = imageData.data[(y * this._width + x) * 4 + 3];
 
-        const alpha = imageData.data[(y * this._width + x) * 4 + 3];
-        const isAlpha = !Boolean(alpha);
-
-        if (alpha !== 0) {
-            if (this._pixelsEqual(selectedColor, color)) return;
+        if (this._pixelsEqual(selectedColor, color)) {
+            return;
         }
 
         const pxlArr: { x: number; y: number }[] = [{ x: x, y: y }];
+        const bpxlArr: { x: number; y: number }[] = [];
         let idx, p;
 
         while (pxlArr.length) {
             p = pxlArr.pop();
 
-            if (p && this._compareColor(imageData, p.x, p.y, selectedColor, isAlpha)) {
+            if (p && this._compareColor(imageData, p.x, p.y, selectedColor, color) === 0) {
                 idx = (p.y * this._width + p.x) * 4;
                 imageData.data[idx] = color[0];
                 imageData.data[idx + 1] = color[1];
                 imageData.data[idx + 2] = color[2];
-                imageData.data[idx + 3] = 255 * color[3];
+                imageData.data[idx + 3] = color[3];
 
-                if (this._compareColor(imageData, p.x, p.y - 1, selectedColor, isAlpha)) {
+                let r;
+                r = this._compareColor(imageData, p.x, p.y - 1, selectedColor, color);
+                if (r === 0) {
                     pxlArr.push({ x: p.x, y: p.y - 1 });
+                } else if (r === 2) {
+                    bpxlArr.push({ x: p.x, y: p.y - 1 });
                 }
-                if (this._compareColor(imageData, p.x + 1, p.y, selectedColor, isAlpha)) {
+                r = this._compareColor(imageData, p.x + 1, p.y, selectedColor, color);
+                if (r === 0) {
                     pxlArr.push({ x: p.x + 1, y: p.y });
+                } else if (r === 2) {
+                    bpxlArr.push({ x: p.x + 1, y: p.y });
                 }
-                if (this._compareColor(imageData, p.x, p.y + 1, selectedColor, isAlpha)) {
+                r = this._compareColor(imageData, p.x, p.y + 1, selectedColor, color);
+                if (r === 0) {
                     pxlArr.push({ x: p.x, y: p.y + 1 });
+                } else if (r === 2) {
+                    bpxlArr.push({ x: p.x, y: p.y + 1 });
                 }
-                if (this._compareColor(imageData, p.x - 1, p.y, selectedColor, isAlpha)) {
+                r = this._compareColor(imageData, p.x - 1, p.y, selectedColor, color);
+                if (r === 0) {
                     pxlArr.push({ x: p.x - 1, y: p.y });
+                } else if (r === 2) {
+                    bpxlArr.push({ x: p.x - 1, y: p.y });
                 }
+            }
+        }
+
+        while (bpxlArr.length) {
+            p = bpxlArr.pop();
+            if (p) {
+                idx = (p.y * this._width + p.x) * 4;
+                imageData.data[idx] = color[0];
+                imageData.data[idx + 1] = color[1];
+                imageData.data[idx + 2] = color[2];
+                imageData.data[idx + 3] = color[3];
             }
         }
 
